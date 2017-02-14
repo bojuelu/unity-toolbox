9c2251413affb9cfef21165a2c2ed74e0570ae71
bojue <bojuelu.mifly@gmail.com>
2017-02-14 15:39:21 +0800

    add script can pause the UGUI event system (can use it to avoid click too frequency cuz many bug)

---
+++ Assets/Scripts/UI framework/PauseEventSystem.cs
@@ -0,0 +1,50 @@
+﻿/// <summary>
+/// Pause event system.
+/// It used for avoid click UI too frequency cuz many weird bug.
+/// Author: BoJue.
+/// </summary>
+
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PauseEventSystem : MonoBehaviour
+{
+    public float pauseTime = 1f;
+    private float timer = 0f;
+
+    private EventSystem eventSystem = null;
+
+    public void Pause()
+    {
+        if (eventSystem.enabled == false)
+            return;
+        else
+        {
+            eventSystem.enabled = false;
+            timer = 0f;
+            this.enabled = true;
+        }
+    }
+
+    private void Awake()
+    {
+        eventSystem = this.gameObject.GetComponent<EventSystem>();
+    }
+
+    private void Start()
+    {
+        this.enabled = false;
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= pauseTime)
+        {
+            eventSystem.enabled = true;
+            this.enabled = false;
+        }
+    }
+}
