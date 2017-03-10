03f3de188d6fe35d6f14fbd7c9d2f64098884a89
bojue <bojuelu.mifly@gmail.com>
2017-03-10 17:42:53 +0800

    sync with workspace

---
+++ Assets/Scripts/UI/Tools/ButtonClickCallback.cs
@@ -0,0 +1,51 @@
+﻿/// <summary>
+/// Button click callback. UGUI Button.onClick has no args let callback function know what button is clicked. This is the solution.
+/// Author: BoJue.
+/// </summary>
+
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ButtonClickCallback : MonoBehaviour
+{
+    public delegate void ClickHandler(ButtonClickCallback b);
+    public event ClickHandler onClickEvent;
+    public string strID = "";
+    public int intID = 0;
+    public float floatID = 0f;
+
+    private Button button;
+    public Button ButtonRef { get { return button; } }
+
+    void Start()
+    {
+        button = this.gameObject.GetComponent<Button>();
+        if (button == null)
+        {
+            Debug.LogError("no commponent 'Button' at this game object. destory itself");
+            onClickEvent = null;
+            GameObject.Destroy(this);
+        }
+        else
+        {
+            button.onClick.AddListener(this.OnClick);
+        }
+    }
+
+    void OnDestroy()
+    {
+        if (button != null)
+        {
+            button.onClick.RemoveListener(this.OnClick);
+        }
+    }
+
+    void OnClick()
+    {
+        if (onClickEvent != null)
+        {
+            onClickEvent.Invoke(this);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Tools/MultiLanguage.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MultiLanguage : MonoBehaviour
+{
+    public UnityEngine.UI.Text[] Texts;
+
+    public string[] english;
+    public string[] chineseTraditional;
+    public string[] chineseSimplified;
+    public string[] japanese;
+    public string[] korean;
+    public string[] thai;
+
+    void Start()
+    {
+        UnityEngine.SystemLanguage lang = Application.systemLanguage;
+        string[] content = null;
+        switch (lang)
+        {
+            case UnityEngine.SystemLanguage.English:
+                content = english;
+                break;
+            case UnityEngine.SystemLanguage.Chinese:
+            case UnityEngine.SystemLanguage.ChineseTraditional:
+                content = chineseTraditional;
+                break;
+            case UnityEngine.SystemLanguage.ChineseSimplified:
+                content = chineseSimplified;
+                break;
+            case SystemLanguage.Japanese:
+                content = japanese;
+                break;
+            case SystemLanguage.Korean:
+                content = korean;
+                break;
+            case SystemLanguage.Thai:
+                content = thai;
+                break;
+            default:
+                content = english;
+                break;
+        }
+
+        for (int i = 0; i < content.Length; i++)
+        {
+            if (content[i] == null)
+                continue;
+            if (Texts[i] == null)
+                continue;
+            if (i >= Texts.Length)
+                break;
+            string theContent = content[i].Replace("\\n", System.Environment.NewLine);
+            Texts[i].text = theContent;
+        }
+    }
+
+}
