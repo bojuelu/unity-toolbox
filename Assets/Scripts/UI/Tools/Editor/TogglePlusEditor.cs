fd1dcbd521c9b31d85a335b2adad6806896375df
bojue <bojuelu.mifly@gmail.com>
2017-03-11 15:28:17 +0800

    add TogglePlus.cs. It can instead of UGUI Toggle

---
+++ Assets/Scripts/UI/Tools/Editor/TogglePlusEditor.cs
@@ -0,0 +1,39 @@
+﻿/// <summary>
+/// Override UGUI ToggleEditor, it's use for TogglePlus.
+/// Inspired by: "http://www.tuicool.com/articles/AbyU7br"
+/// Author: BoJue.
+/// </summary>
+
+using UnityEngine;  
+using System.Collections;  
+using UnityEditor;  
+using UnityEditor.UI;
+
+[CustomEditor (typeof(TogglePlus), true)]
+[CanEditMultipleObjects]
+
+public class TogglePlusEditor : ToggleEditor
+{
+    SerializedProperty onImage;
+    SerializedProperty offImage;
+
+    protected override void OnEnable ()
+    {
+        base.OnEnable ();
+        onImage = serializedObject.FindProperty("onImage");
+        offImage = serializedObject.FindProperty("offImage");
+    }
+
+
+    public override void OnInspectorGUI ()
+    {
+        base.OnInspectorGUI ();
+
+        EditorGUILayout.Space ();
+
+        serializedObject.Update ();
+        EditorGUILayout.PropertyField(onImage, true);
+        EditorGUILayout.PropertyField(offImage, true);
+        serializedObject.ApplyModifiedProperties ();
+    }
+}
---
+++ Assets/Scripts/UI/Tools/TogglePlus.cs
@@ -0,0 +1,25 @@
+﻿/// <summary>
+/// Instead of UGUI Toggle isOn using one graphic show or not show. Here use two graphic switch each other.
+/// Author: BuJue.
+/// </summary>
+
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TogglePlus : Toggle
+{
+    public Graphic onImage;
+    public Graphic offImage;
+
+    void Update()
+    {
+        if (graphic != null)
+            graphic.enabled = false;
+
+        if (onImage != null)
+            onImage.enabled = isOn;
+        if (offImage != null)
+            offImage.enabled = !isOn;
+    }
+}
