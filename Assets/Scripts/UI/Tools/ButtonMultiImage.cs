10e09bbd009b5f07f588dd1c04f6589c1e57a748
bojue <bojuelu.mifly@gmail.com>
2017-03-31 11:28:03 +0800

    add tool

---
+++ Assets/Scripts/UI/Tools/ButtonMultiImage.cs
@@ -0,0 +1,71 @@
+﻿/// <summary>
+/// Make multi-image trasnition.
+/// Author: Riposte. website: http://answers.unity3d.com/users/254200/riposte.html
+/// Refrence: http://answers.unity3d.com/questions/820311/ugui-multi-image-button-transition.html
+/// </summary>
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class ButtonMultiImage : Button
+{
+    private Graphic[] m_graphics;
+    protected Graphic[] Graphics
+    {
+        get
+        {
+            if(m_graphics == null)
+            {
+                m_graphics = targetGraphic.transform.parent.GetComponentsInChildren<Graphic>();
+            }
+            return m_graphics;
+        }
+    }
+
+    protected override void DoStateTransition (SelectionState state, bool instant)
+    {
+        Color color;
+        switch (state)
+        {
+        case Selectable.SelectionState.Normal:
+            color = this.colors.normalColor;
+            break;
+        case Selectable.SelectionState.Highlighted:
+            color = this.colors.highlightedColor;
+            break;
+        case Selectable.SelectionState.Pressed:
+            color = this.colors.pressedColor;
+            break;
+        case Selectable.SelectionState.Disabled:
+            color = this.colors.disabledColor;
+            break;
+        default:
+            color = Color.black;
+            break;
+        }
+        if (base.gameObject.activeInHierarchy)
+        {
+            switch (this.transition)
+            {
+            case Selectable.Transition.ColorTint:
+                ColorTween (color * this.colors.colorMultiplier, instant);
+                break;
+            default:
+                throw new NotSupportedException();
+            }
+        }
+    }
+
+    private void ColorTween(Color targetColor, bool instant)
+    {
+        if (this.targetGraphic == null)
+        {
+            return;
+        }
+
+        foreach(Graphic g in this.Graphics)
+        {
+            g.CrossFadeColor (targetColor, (!instant) ? this.colors.fadeDuration : 0f, true, true);
+        }
+    }
+}
