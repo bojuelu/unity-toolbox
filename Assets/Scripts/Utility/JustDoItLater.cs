608a8eedac239880db7cf77be210a70aa9754bc5
bojue <bojuelu.mifly@gmail.com>
2017-04-05 12:29:47 +0800

    sync from workspace

--- Assets/Scripts/Utility/JustDoItLater.cs
+++ Assets/Scripts/Utility/JustDoItLater.cs
@@ -1,73 +1,27 @@
 ﻿/// <summary>
 /// Just do it ...later! "http://picture-cdn.wheretoget.it/crgqai-l.jpg"
-/// Sometimes, we want to do something, but it should wait for a while, so... just do it later!
-/// It's another option between MonoBehavior.StartCorutine().
-/// Author: BoJue.
-/// 
-/// Example:
-/// public class MyPokemonAction : MonoBehaviour
-/// {
-///     public Animation snorlax;
-///     public AnimationClip yawn;
-///     public AnimationClip lieDown;
-/// 
-///     // The Snorlax is going to sleep, 
-///     // she make a yawn, then lie down for sleep.
-///     public void GoToSleep()
-///     {
-///         // play yawn directly.
-///         PlayClip(snorlax, yawn);
-/// 
-///         // just lie down! ...later. bcz you should wait for yawn action complete.
-///         JustDoItLater justDoItLater = snorlax.gameObject.AddComponent<JustDoItLater>();
-///         object[] things = new object[2];
-///         things[0] = snorlax as object;
-///         things[1] = lieDown as object;
-///         justDoItLater.justDo = OnPlayClip;  // just do: use animation to play the clip
-///         justDoItLater.it = things;  // it: the animation and clip
-///         justDoItLater.later = 5f;  // later: in this case, yawn action spend 5 seconds.
-///     }
-/// 
-///     private void PlayClip(Animation character, AnimationClip clip)
-///     {
-///         character.clip = clip;
-///         character.Play();
-///     }
-/// 
-///     private object OnPlayClip(object[] objs)
-///     {
-///         Animation snorlax = objs[0] as Animation;
-///         AnimationClip lieDown = objs[1] as AnimationClip;
-///         PlayClip(snorlax, lieDown);
-///         return null;
-///     }
-/// }
 /// </summary>
 
 using UnityEngine;
-using System.Collections;
+using UnityEngine.Events;
 
 public class JustDoItLater : MonoBehaviour
 {
-    public delegate object Method(object[] objs);
-    public Method justDo = null;
-    public object[] it = null;
-    public float later = 0f;
-
+    public UnityEvent justDoIt;
+    public float later = 1f;
     private float t = 0f;
 
     void Update()
     {
-        t += Time.deltaTime;
-        if (t < later)
+        if (t >= later)
         {
-            return;
+            justDoIt.Invoke();
+            justDoIt.RemoveAllListeners();
+            GameObject.Destroy(this);
         }
         else
         {
-            if (justDo != null)
-                justDo.Invoke(it);
-            GameObject.Destroy(this);
+            t += Time.deltaTime;
         }
     }
 }
