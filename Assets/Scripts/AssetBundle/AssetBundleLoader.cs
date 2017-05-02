d79b9c1c426fd13721ba66adb52314bcd26236f7
bojue <bojuelu.mifly@gmail.com>
2017-05-02 15:39:18 +0800

    1. sync iTween.cs form workspace, fix compile error 2. add AB tools

---
+++ Assets/Scripts/AssetBundle/AssetBundleLoader.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AssetBundleLoader : MonoBehaviour
+{
+    public bool DrawTestingButton = false;
+    public string FilePath = "";
+    public event System.EventHandler BuildCompleteEvent;
+    
+    private WWW _wwwObj = null;
+    private GameObject _buildedObject = null;
+
+    IEnumerator Build()
+    {
+        if (_wwwObj != null)
+        {
+            _wwwObj.Dispose();
+            _wwwObj = null;
+        }
+
+        _wwwObj = new WWW(FilePath);
+
+        while (!_wwwObj.isDone)
+        {
+            Debug.Log("building progress: " + (int)(_wwwObj.progress * 100f) + "%");
+            yield return null;
+        }
+        yield return new WaitForEndOfFrame();
+        Debug.Log("building progress: " + 100f + "%");
+
+        if (string.IsNullOrEmpty(_wwwObj.error))
+        {
+            AssetBundle ab = _wwwObj.assetBundle;
+            Object mainAsset = ab.mainAsset;
+
+            _buildedObject = GameObject.Instantiate(mainAsset) as GameObject;
+            if (BuildCompleteEvent != null)
+            {
+                BuildCompleteEvent(_buildedObject, null);
+            }
+        }
+        else
+        {
+            Debug.LogError(_wwwObj.error);
+        }
+    }
+
+    // test
+    void OnGUI()
+    {
+        if (!DrawTestingButton)
+            return;
+        
+        FilePath = GUI.TextField(new Rect(0, 0, 1024, 100), FilePath);
+
+        if (GUI.Button(new Rect(0, 100, 100, 100), "build"))
+        {
+            this.StopAllCoroutines();
+            this.StartCoroutine(this.Build());
+        }
+    }
+}
---
+++ Assets/Scripts/AssetBundle/Editor/AssetBundleBuilder.cs
@@ -0,0 +1,74 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AssetBundleBuilder
+{
+    const string kMenu = "Assets/Build AssetBundles/";
+
+    [MenuItem(kMenu + "Android")]
+    static void CreateAssetBunldesForAndroid()
+    {
+        ExecCreateAssetBunldes(BuildTarget.Android);
+    }
+
+    [MenuItem(kMenu + "iOS")]
+    static void CreateAssetBunldesForIOS()
+    {
+        ExecCreateAssetBunldes(BuildTarget.iOS);
+    }
+
+    static void ExecCreateAssetBunldes(BuildTarget buildTarget)
+    {
+        if (EditorApplication.isPlaying || EditorApplication.isPaused)
+        {
+            Debug.LogWarning("Stop the editor first");
+            return;
+        }
+
+        string targetDir = "AssetBundles/" + buildTarget.ToString();
+        string extensionName = ".asset";
+
+        if (!Directory.Exists(targetDir))
+            Directory.CreateDirectory(targetDir);
+        
+        Object[] selectedAsset = Selection.GetFiltered(typeof(Object), SelectionMode.DeepAssets);
+
+        Debug.Log("Select object count: " + selectedAsset.Length);
+        Debug.Log("Object list:");
+        for (int i = 0; i < selectedAsset.Length; i++)
+        {
+            Object obj = selectedAsset[i];
+            Debug.Log(obj.name);
+        }
+
+        for (int i = 0; i < selectedAsset.Length; i++)
+        {
+            Object obj = selectedAsset[i];
+
+            //string sourcePath = AssetDatabase.GetAssetPath(obj);
+            string targetPath = targetDir + Path.DirectorySeparatorChar + obj.name + extensionName;
+
+            if (File.Exists(targetPath))
+                File.Delete(targetPath);
+
+            if (!(obj is GameObject) && !(obj is Texture2D) && !(obj is Material))
+            {
+                Debug.LogWarning(obj.name + " can not build for assetbundle, skip it.");
+                continue;
+            }
+
+            if (BuildPipeline.BuildAssetBundle(obj, null, targetPath, BuildAssetBundleOptions.CollectDependencies, buildTarget))
+            {
+                Debug.Log(targetPath + " build complete");
+            }
+            else
+            {  
+                Debug.Log(targetPath + " build failed");
+            }
+        }
+    }
+
+}
