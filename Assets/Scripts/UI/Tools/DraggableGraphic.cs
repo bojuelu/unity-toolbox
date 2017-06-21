2425b1fdcf1d865b1426e11a1c4943cdb27a617c
bojue <bojuelu.mifly@gmail.com>
2017-06-21 18:09:31 +0800

    add 2d tool

---
+++ Assets/Scripts/UI/Tools/DraggableGraphic.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(Graphic))]
+public class DraggableGraphic : MonoBehaviour, IDragHandler
+{
+    private RectTransform thisRectTransform;
+
+    void Start()
+    {
+        gameObject.GetComponent<Graphic>().raycastTarget = true;
+        thisRectTransform = gameObject.GetComponent<RectTransform>();
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (thisRectTransform == null)
+        {
+            Debug.LogError("[DraggableGraphic] thisRectTransform == null");
+            return;
+        }
+        Vector3 globalMousePos;
+        if (RectTransformUtility.ScreenPointToWorldPointInRectangle(
+            thisRectTransform,
+            eventData.position,
+            eventData.pressEventCamera,
+            out globalMousePos)
+        )
+        {
+            thisRectTransform.position = globalMousePos;
+        }
+    }
+}
