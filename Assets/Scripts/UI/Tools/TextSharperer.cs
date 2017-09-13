1d9bf909fca80eea19285a635f78122d4fbaf781
bojue <bojuelu@miflydesign.com>
2017-09-13 16:13:41 +0800

    update new tool

---
+++ Assets/Scripts/UI/Tools/TextSharperer.cs
@@ -0,0 +1,29 @@
+﻿/// <summary>
+/// Sharp the fucking UGUI Text
+/// Refrence: http://answers.unity3d.com/questions/1226551/ui-text-is-blurred-unity-535f.html
+/// Use the way "Lame hack 1"
+/// Author: U_Ku_Shu (http://answers.unity3d.com/users/490552/u-ku-shu.html)
+/// </summary>
+
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class TextSharperer : MonoBehaviour
+{
+    private const int scaleValue = 10;
+
+    private Text thisText;
+
+    void Start ()
+    {
+        thisText = gameObject.GetComponent<Text>();
+
+        thisText.fontSize = thisText.fontSize * scaleValue;
+
+        thisText.transform.localScale = thisText.transform.localScale / scaleValue;
+
+        thisText.horizontalOverflow = HorizontalWrapMode.Overflow;
+        thisText.verticalOverflow = VerticalWrapMode.Overflow;
+    }
+}
