42e5a20131c5a5682843663f8a3c7af53dc8f307
bojue <bojuelu@miflydesign.com>
2017-11-13 22:38:03 +0800

    At iOS platform, it can switch log enable/disable at Xcode via GetLogTagClass.mm

--- Assets/Plugins/OverwriteDebugLog/Debug.cs
+++ Assets/Plugins/OverwriteDebugLog/Debug.cs
@@ -1,17 +1,146 @@
 ﻿/// <summary>
 /// Hide Debug log when product release.
-/// Use Unity: Build Settings / Scripting Define Symbols (or define in your code), to decide what kind of log you want to show.
-/// Define Symbols: LOG_VERBOSE, LOG_DEBUG, LOG_WARNING, LOG_ERROR, LOG_EXCEPTION .
+/// Log tags: LOG_VERBOSE, LOG_DEBUG, LOG_WARNING, LOG_ERROR, LOG_EXCEPTION .
 /// LOG_VERBOSE will show all kind of logs.
 /// Author: BoJue.
 /// </summary>
 
-
-#if !UNITY_EDITOR
-#define HIDE_LOG
+#if UNITY_EDITOR
+// In Unity editor, this script is no use, print log as usual.
+#else
+    #if UNITY_IOS
+        // At iOS, use GetLogTagClass.mm to setup log tag
+        #define HIDE_LOG_FROM_XCODE
+    #else
+        // At the other OS, goto: Build Settings -> Player Settings -> Other Settings -> Scripting Define Symbols. to setup log tag
+        #define HIDE_LOG
+    #endif
 #endif
 
-#if HIDE_LOG
+#if HIDE_LOG_FROM_XCODE
+using System.Runtime.InteropServices;
+
+public static class Debug
+{
+    [DllImport("__Internal")]
+    private static extern string GetLogTag();
+
+    public static void Log(object o)
+    {
+        if (GetLogTag() == "LOG_VERBOSE" || GetLogTag() == "LOG_DEBUG")
+            UnityEngine.Debug.Log(o);
+    }
+
+    public static void Log(object o, UnityEngine.Object uo)
+    {
+        if (GetLogTag() == "LOG_VERBOSE" || GetLogTag() == "LOG_DEBUG")
+            UnityEngine.Debug.Log(o, uo);
+    }
+
+    public static void LogFormat(string format, params object[] args)
+    {
+        if (GetLogTag() == "LOG_VERBOSE" || GetLogTag() == "LOG_DEBUG")
+            UnityEngine.Debug.LogFormat(format, args);
+    }
+
+    public static void LogWarning(object o)
+    {
+        if (GetLogTag() == "LOG_VERBOSE" || GetLogTag() == "LOG_WARNING")
+            UnityEngine.Debug.LogWarning(o);
+    }
+
+    public static void LogWarning(object o, UnityEngine.Object uo)
+    {
+        if (GetLogTag() == "LOG_VERBOSE" || GetLogTag() == "LOG_WARNING")
+            UnityEngine.Debug.LogWarning(o, uo);
+    }
+
+    public static void LogWarningFormat(string format, params object[] args)
+    {
+        if (GetLogTag() == "LOG_VERBOSE" || GetLogTag() == "LOG_WARNING")
+            UnityEngine.Debug.LogWarningFormat(format, args);
+    }
+
+    public static void LogError(object o)
+    {
+        if (GetLogTag() == "LOG_VERBOSE" || GetLogTag() == "LOG_ERROR")
+            UnityEngine.Debug.LogError(o);
+    }
+
+    public static void LogError(object o, UnityEngine.Object uo)
+    {
+        if (GetLogTag() == "LOG_VERBOSE" || GetLogTag() == "LOG_ERROR")
+            UnityEngine.Debug.LogError(o, uo);
+    }
+
+    public static void LogErrorFormat(string format, params object[] args)
+    {
+        if (GetLogTag() == "LOG_VERBOSE" || GetLogTag() == "LOG_ERROR")
+            UnityEngine.Debug.LogErrorFormat(format, args);
+    }
+
+    public static void LogException(System.Exception e)
+    {
+        if (GetLogTag() == "LOG_VERBOSE" || GetLogTag() == "LOG_ERROR")
+            UnityEngine.Debug.LogException(e);
+    }
+
+    public static void LogException(System.Exception e, UnityEngine.Object uo)
+    {
+        if (GetLogTag() == "LOG_VERBOSE" || GetLogTag() == "LOG_ERROR")
+            UnityEngine.Debug.LogException(e, uo);
+    }
+
+    public static void Assert(bool b)
+    {
+        //        UnityEngine.Debug.Assert(b);
+    }
+
+    public static void Assert(bool b, UnityEngine.Object o)
+    {
+        //        UnityEngine.Debug.Assert(b, o);
+    }
+
+    public static void Assert(bool b, object o)
+    {
+        //        UnityEngine.Debug.Assert(b, o);
+    }
+
+    public static void Assert(bool b, string s)
+    {
+        //        UnityEngine.Debug.Assert(b, s);
+    }
+
+    public static void Assert(bool b, object o, UnityEngine.Object c)
+    {
+        //        UnityEngine.Debug.Assert(b, o, c);
+    }
+
+    public static void Assert(bool b, string s, UnityEngine.Object c)
+    {
+        //        UnityEngine.Debug.Assert(b, s, c);
+    }
+
+    public static void Assert(bool b, string s, params object[] args)
+    {
+    }
+
+    public static void DrawRay(UnityEngine.Vector3 start, UnityEngine.Vector3 dir)
+    {
+        UnityEngine.Debug.DrawRay(start, dir);
+    }
+
+    public static void DrawLine(UnityEngine.Vector3 start, UnityEngine.Vector3 end)
+    {
+        UnityEngine.Debug.DrawLine(start, end);            
+    }
+
+    public static void DebugBreak()
+    {
+        UnityEngine.Debug.DebugBreak();
+    }
+}
+#elif HIDE_LOG
 public static class Debug
 {
     public static void Log(object o)
---
+++ Assets/Plugins/iOS/GetLogTagClass.mm
@@ -0,0 +1,41 @@
+// Refrence from: https://stackoverflow.com/questions/37047781/how-to-return-string-from-native-ios-plugin-to-unity
+// Author: BoJue
+// Orig-Author: Cabrra https://stackoverflow.com/users/6162065/cabrra
+// Orig-Author say, He found code from: http://blog.mediarain.com/2013/03/creating-ios-plugins-for-unity/
+
+@implementation GetLogTagClass : UIViewController
+
+@end
+
+char* cStringCopy(const char* string)
+{
+    if (string == NULL)
+        return NULL;
+    
+    char* res = (char*)malloc(strlen(string) + 1);
+    strcpy(res, string);
+    
+    return res;
+}
+
+// This takes a char* you get from Unity and converts it to an NSString* to use in your objective c code. You can mix c++ and objective c all in the same file.
+static NSString* CreateNSString(const char* string)
+{
+    if (string != NULL)
+        return [NSString stringWithUTF8String:string];
+    else
+        return [NSString stringWithUTF8String:""];
+}
+
+
+
+extern "C"
+{
+    char* GetLogTag()
+    {
+        NSString *logTag = @"LOG_NONE";
+        
+        return cStringCopy([logTag UTF8String]);
+    }
+}
+
