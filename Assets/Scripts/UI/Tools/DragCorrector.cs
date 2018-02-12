a4861e08cc8a79f3d8cefa6d727cd1d55220d03a
bojue <bojuelu@miflydesign.com>
2018-02-12 19:45:25 +0800

    Add new UI tool, It can fix the issue: High resolution device is too sensitive on drag, so let click event being ignored.

---
+++ Assets/Scripts/UI/Tools/DragCorrector.cs
@@ -0,0 +1,26 @@
+﻿/// <summary>
+/// Fix issue: High resolution device is too sensitive on drag, so let click event being ignored.
+/// https://answers.unity.com/questions/1149417/ui-button-onclick-sensitivity-for-high-dpi-devices.html
+/// Author: FireOApache (https://answers.unity.com/users/323653/fireoapache.html)
+/// </summary>
+
+using UnityEngine;
+using UnityEngine.EventSystems;
+ 
+public class DragCorrector : MonoBehaviour
+{
+    public int baseTH = 6;
+    public int basePPI = 210;
+    public int dragTH = 0;
+
+    void Start()
+    {
+        dragTH = baseTH * (int)Screen.dpi / basePPI;
+
+        EventSystem es = GetComponent<EventSystem>();
+
+        if (es) es.pixelDragThreshold = dragTH;
+
+        Debug.Log("[DragCorrector] dragTH: " + dragTH);
+    }
+}
