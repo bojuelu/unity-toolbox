f371b832ff0443437af1387cc50859fc53090836
bojue <bojuelu@miflydesign.com>
2018-04-13 10:48:43 +0800

    add new UI tool: Via swap GameObject to perform Button 4 status (normal, hover, select, disable)

---
+++ Assets/Scripts/UI/Tools/ButtonObjectSwap.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UnityToolbox
+{
+    /// <summary>
+    /// Button object swap.
+    /// Author: BoJue.
+    /// </summary>
+    public class ButtonObjectSwap : Button
+    {
+        public GameObject normal;
+        public GameObject highlighted;
+        public GameObject pressed;
+        public GameObject disabled;
+
+        protected override void DoStateTransition(SelectionState state, bool instant)
+        {
+            base.DoStateTransition(state, instant);
+
+            switch (state)
+            {
+                case Selectable.SelectionState.Normal:
+                    normal.SetActive(true);
+                    highlighted.SetActive(false);
+                    pressed.SetActive(false);
+                    disabled.SetActive(false);
+                    break;
+                case Selectable.SelectionState.Highlighted:
+                    normal.SetActive(false);
+                    highlighted.SetActive(true);
+                    pressed.SetActive(false);
+                    disabled.SetActive(false);
+                    break;
+                case Selectable.SelectionState.Pressed:
+                    normal.SetActive(false);
+                    highlighted.SetActive(false);
+                    pressed.SetActive(true);
+                    disabled.SetActive(false);
+                    break;
+                case Selectable.SelectionState.Disabled:
+                    normal.SetActive(false);
+                    highlighted.SetActive(false);
+                    pressed.SetActive(false);
+                    disabled.SetActive(true);
+                    break;
+                default:
+                    normal.SetActive(true);
+                    highlighted.SetActive(false);
+                    pressed.SetActive(false);
+                    disabled.SetActive(false);
+                    break;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Tools/Editor/ButtonObjectSwapEditor.cs
@@ -0,0 +1,42 @@
+﻿using UnityEditor;
+using UnityEditor.UI;
+
+namespace UnityToolbox
+{
+    /// <summary>
+    /// Button object swap editor.
+    /// Author: BoJue.
+    /// </summary>
+    [CustomEditor(typeof(ButtonObjectSwap), true)]
+    [CanEditMultipleObjects]
+    public class ButtonObjectSwapEditor : ButtonEditor
+    {
+        SerializedProperty normal;
+        SerializedProperty highlighted;
+        SerializedProperty pressed;
+        SerializedProperty disabled;
+
+        protected override void OnEnable()
+        {
+        	base.OnEnable();
+        	normal = serializedObject.FindProperty("normal");
+        	highlighted = serializedObject.FindProperty("highlighted");
+            pressed = serializedObject.FindProperty("pressed");
+            disabled = serializedObject.FindProperty("disabled");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            EditorGUILayout.Space();
+
+            serializedObject.Update();
+            EditorGUILayout.PropertyField(normal);
+            EditorGUILayout.PropertyField(highlighted);
+            EditorGUILayout.PropertyField(pressed);
+            EditorGUILayout.PropertyField(disabled);
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
