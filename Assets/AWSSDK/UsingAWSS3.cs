de28e1ebad2cb868d481d5d64d90bc2aa591a9fd
bojue <bojuelu@miflydesign.com>
2018-06-26 13:52:00 +0800

    Upload demo code

---
+++ Assets/AWSSDK/UsingAWSS3.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UsingAWSS3 : MonoBehaviour
+{
+    public S3Function weidunS3;
+
+    void Start()
+    {
+        weidunS3.setIdentityPoolId("ab-cdefghijk-l:12345678-1234-1234-1234-123456789012");
+        weidunS3.setCognitoIdentityRegion("ab-cdefghijk-l");
+        weidunS3.setS3Region("ab-cdefghijk-l");
+        weidunS3.setBucketName("aaaa.test.accessable");
+    }
+
+    public void PostObj()
+    {
+        string postObj = "/Users/yuyuhashow/Downloads/0615/testyo.txt";
+        Debug.Log("post obj:" + postObj);
+        Debug.Log("file exists? =" + System.IO.File.Exists(postObj));
+        Debug.Log(
+            string.Format(
+                "{0}\n{1}\n{2}\n{3}",
+                weidunS3.GetIdentityPoolId,
+                weidunS3.GetCognitoIdentityRegion,
+                weidunS3.GetS3Region,
+                weidunS3.GetS3BucketName
+            )
+        );
+        weidunS3.PostObject("ABC/", postObj, "file1.txt", (string strResult) =>
+            {
+                Debug.Log(strResult);
+            });
+    }
+}
---
+++ Assets/AliyunSDK/UsingAliyunOss.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AliyunSDK;
+
+public class UsingAliyunOss : MonoBehaviour
+{
+    public OssAgent ossAgent;
+
+    double t1;
+    double t2;
+
+    public void Start()
+    {
+        ossAgent.Init("ooo-us-hollywood.mumuhaha.com", "ABCDEFGHIJKL", "abcdABCDabcdABCDabcd", "my-buckett");
+        ossAgent.onPutObjectDone += OnUploadDone;
+    }
+
+    void OnDestroy()
+    {
+        ossAgent.onPutObjectDone -= OnUploadDone;
+    }
+
+    public void UploadFile()
+    {
+        t1 = System.DateTime.UtcNow.Subtract(new System.DateTime(1970, 1, 1)).TotalSeconds;
+        ossAgent.PutObjectByNewThread("/Users/yuyuhashow/Downloads/test_large_file.txt", System.DateTime.Now.ToString() + ".file");
+    }
+
+    private void OnUploadDone(string url)
+    {
+        t2 = System.DateTime.UtcNow.Subtract(new System.DateTime(1970, 1, 1)).TotalSeconds;
+        Debug.Log("OnUploadDone");
+        Debug.Log("Spend=" + (t2 - t1));
+        Debug.Log(url);
+    }
+}
